347ff608b9e5e8346cde713543681ea27bbfb411
medjedshin <yutabreaststroker0504@gmail.com>
2018-11-27 20:35:01 +0900

    Add reset function

--- js/main.js
+++ js/main.js
@@ -1,30 +1,6 @@
 (function () {
   'use strict';
-  
-  var board = document.getElementById('board');
-
-  // 盤面の初期状態を作る関数
-  function makeBoard(table, N) {
-    for (var i=0; i < N; i++) {
-      var row = table.insertRow(-1);
-      for (var j=0; j < N; j++) {
-        var id = 'square' + String(i) + String(j);
-        if (i === 0) {
-          var th = document.createElement('th');
-          th.classList.add('sq-front');
-          th.setAttribute('id', id);
-          row.appendChild(th);
-        } else {
-          var td = document.createElement('td');
-          td.classList.add('sq-front');
-          td.setAttribute('id', id);
-          row.appendChild(td);
-        }
-      }
-    }
-    board.appendChild(table);  
-  }
-
+    
   // 盤面に爆弾を配置する関数
   function makeState(N, bombNum) {
     // 爆弾の位置を決定
@@ -77,38 +53,82 @@
     return states;
   }
 
-   /* var states = [['', '1', '1'],
-   *               ['1', 'bomb', '1'],
-   *               ['1', '1', '1']]; */
-
-  var N = 3;
-  var table = document.createElement('table');
-  makeBoard(table, N);
-  var states = makeState(3, 1);
-
-
-  // click
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      var square = document.getElementById('square' + String(i) + String(j));
-            
-      square.addEventListener('click', function() {
-        var state = states[i][j];
-        this.classList.remove('sq-front');
-              
-        if (state === 'bomb') {
-          this.classList.add('bomb');
-          var img = document.createElement('img');
-          img.setAttribute('src', './images/bomb.png');
-          img.setAttribute('width', '30px');
-          img.setAttribute('height', '30px');
-          this.appendChild(img);
+  // 盤面の初期状態を作る関数
+  function initBoard(N) {
+    var table = document.createElement('table');
+    table.setAttribute('id', 'tbl');
+    
+    for (var i=0; i < N; i++) {
+      var row = table.insertRow(-1);
+      for (var j=0; j < N; j++) {
+        var id = 'square' + String(i) + String(j);
+        if (i === 0) {
+          var th = document.createElement('th');
+          th.classList.add('sq-front');
+          th.setAttribute('id', id);
+          row.appendChild(th);
         } else {
-          this.classList.add('sq-back');
-          this.textContent = state;
-        }        
-      });
+          var td = document.createElement('td');
+          td.classList.add('sq-front');
+          td.setAttribute('id', id);
+          row.appendChild(td);
+        }
+      }
     }
+    board.appendChild(table);  
   }
+
+  var board = document.getElementById('board');
+  var gameOver = document.getElementById('gameover');
+  var reset = document.getElementById('reset');
+
+  function main() {
+  
+    var N = 3;
+    var bombNum = 1;
+    
+    initBoard(N);
+    var states = makeState(N, bombNum);
+    
+    // click
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        var square = document.getElementById('square' + String(i) + String(j));
+        
+        square.addEventListener('click', function() {
+          var state = states[i][j];
+          this.classList.remove('sq-front');
+          
+          if (state === 'bomb') {
+            this.classList.add('bomb');
+            var img = document.createElement('img');
+            img.setAttribute('src', './images/bomb.png');
+            img.setAttribute('width', '30px');
+            img.setAttribute('height', '30px');
+            this.appendChild(img);
+            gameOver.classList.remove('hidden');
+            reset.classList.remove('hidden');
+          } else {
+            this.classList.add('sq-back');
+            this.textContent = state;
+          }        
+        });
+      }
+    }
+    
+    // reset
+    reset.addEventListener('click', function() {    
+      gameOver.classList.add('hidden');
+      reset.classList.add('hidden');
+      // table削除
+      var tbl = document.getElementById('tbl');
+      var tblParent = tbl.parentNode;
+      tblParent.removeChild(tbl);
+      
+      main();
+    });
+  }
+  
+  main();
   
 })();
